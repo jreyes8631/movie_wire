4872260a15d453c16c4e8f5b612b62498c1fe2fe
jreyes8631 <jreyes8631@gmail.com>
2020-11-07 01:06:55 -0500

    add crud action for movie and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,54 @@
 class MoviesController < ApplicationController
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
     def index 
-       @movie = Movie.find(params[:id])
+       @movies = Movie.all
+    end
+
+    def new
+       @movie = Movie.new
     end
     
+    def show
+      @movies = Movie.all
+    end
+
+    def edit
+
+    end
+
+    def create
+      @movie = current_user.movies.build(movie_params)
+      if @movie.save
+        redirect_to movie_path(@movie)
+      else
+        render :new
+      end
+    end
+
+    def update
+       @movie.update(movie_params)
+       if @movie.update!
+        redirect_to movie_path(@movie)
+       else
+        render :edit 
+       end
+    end
+
+    def destroy
+     @movie.destroy
+     redirect_to movies_path
+    end
+
+
+    private
+
+    def set_movie
+       @movie = Movie.find_by(id: params[:id])
+    end
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :movie_length, :director, :rating)
+    end
+
 end
