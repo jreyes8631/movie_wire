90aab3b95308ff5b485b59be333f767fe2802934
jreyes8631 <jreyes8631@gmail.com>
2020-11-07 16:39:52 -0500

    add root to movie#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_001426) do
+ActiveRecord::Schema.define(version: 2020_11_07_151247) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2020_11_07_001426) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
