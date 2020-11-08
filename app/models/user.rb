87e0c7b4d86700633367cd888aedbc6d5364a2ff
jreyes8631 <jreyes8631@gmail.com>
2020-11-08 10:03:51 -0500

    fix associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
   has_many :movies
   has_many :reviews, dependent: :destroy
-  has_many :reviews, through: :movies
+  has_many :reviewed_movies, through: :reviews, source: :movie
 end
